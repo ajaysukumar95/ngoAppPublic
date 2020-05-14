aa7c8c98b4a236dbf0a52fffc2af69914103213d
ajaysukumar95 <64750662+ajaysukumar95@users.noreply.github.com>
2020-05-15 01:09:39 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- <v-app :class="{ 'bg': $route.path === '/login' }"> -->
+  <v-app :class="bgClassadd()">
+   <router-view></router-view>
+  </v-app>
+</template>
+
+<style>
+.bg{
+  background:url(./assets/Bg2.jpg)!important;
+   background-position: center !important;
+    background-repeat: no-repeat !important;
+    background-size: cover !important;
+}
+a{
+  text-decoration: none!important;
+}
+</style>
+<script>
+export default {
+  name: "App",
+data(){
+  return{
+   
+}
+
+},
+methods:{
+      bgClassadd () {
+      if (this.$route.name === 'login') {
+      
+        return 'bg' 
+      }
+      else {
+
+        return this.cc
+      }  
+    }
+},
+// mounted(){
+//   debugger;
+//   bgClassadd ()
+// }
+};
+</script>
